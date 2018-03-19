c5a329647d78ef4e9a73678bc006bc4f35796e25
Safisyx <safisyx@gmail.com>
2018-03-19 17:03:40 +0100

    Add user, player and game entities

---
+++ src/games/entities.ts
@@ -0,0 +1,59 @@
+import { BaseEntity, PrimaryGeneratedColumn, Column, Entity, Index, OneToMany, ManyToOne } from 'typeorm'
+import User from '../users/entity'
+
+export type Row = [ string , string ,string ,string ,string ,string ,string ,string ,string ,string]
+export type Board = [ Row, Row, Row, Row, Row, Row,Row, Row, Row, Row]
+
+type Status = 'pending' | 'started' | 'finished'
+
+//const emptyRow: Row = [null, null, null]
+const emptyBoard: Board = [
+  ['0','0','0','0','0','0','0','0','0','0'],
+  ['0','0','0','0','0','0','0','0','0','0'],
+  ['0','0','0','0','0','0','0','0','0','0'],
+  ['0','0','0','0','0','0','0','0','0','0'],
+  ['0','0','0','0','0','0','0','0','0','0'],
+  ['0','0','0','0','0','0','0','0','0','0'],
+  ['0','0','0','0','0','0','0','0','0','0'],
+  ['0','0','0','0','0','0','0','0','0','0'],
+  ['0','0','0','0','0','0','0','0','0','0'],
+  ['0','0','0','0','0','0','0','0','0','0']
+]
+
+@Entity()
+export class Game extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @Column('json', {default: emptyBoard})
+  board1: Board
+
+  @Column('json', {default: emptyBoard})
+  board2: Board
+
+  @Column('text', {default: 'pending'})
+  status: Status
+
+  // this is a relation, read more about them here:
+  // http://typeorm.io/#/many-to-one-one-to-many-relations
+  @OneToMany(_ => Player, player => player.game, {eager:true})
+  players: Player[]
+}
+
+@Entity()
+@Index(['game', 'user'], {unique:true})
+export class Player extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @ManyToOne(_ => User, user => user.players)
+  user: User
+
+  @ManyToOne(_ => Game, game => game.players)
+  game: Game
+
+  @Column()
+  userId: number
+}
--- target/db.js
+++ target/db.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
 const DefaultNamingStrategy_1 = require("typeorm/naming-strategy/DefaultNamingStrategy");
 const StringUtils_1 = require("typeorm/util/StringUtils");
+const entity_1 = require("./users/entity");
+const entities_1 = require("./games/entities");
 class CustomNamingStrategy extends DefaultNamingStrategy_1.DefaultNamingStrategy {
     tableName(targetName, userSpecifiedName) {
         return userSpecifiedName ? userSpecifiedName : StringUtils_1.snakeCase(targetName) + 's';
@@ -20,7 +22,11 @@ class CustomNamingStrategy extends DefaultNamingStrategy_1.DefaultNamingStrategy
 exports.default = () => typeorm_1.createConnection({
     type: "postgres",
     url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
-    entities: [],
+    entities: [
+        entity_1.default,
+        entities_1.Player,
+        entities_1.Game
+    ],
     synchronize: true,
     logging: true,
     namingStrategy: new CustomNamingStrategy()
