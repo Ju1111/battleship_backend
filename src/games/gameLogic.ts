356db82beba33f20c0f9ebc6215d175776aa4931
Safisyx <safisyx@gmail.com>
2018-03-19 21:17:34 +0100

    Add types to the agruments of the functions

--- src/games/gameLogic.ts
+++ src/games/gameLogic.ts
@@ -1,5 +1,5 @@
 import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator'
-import { Board, emptyBoard} from './entities';
+import { Board, emptyBoard, Row} from './entities';
 
 @ValidatorConstraint()
 export class IsBoard implements ValidatorConstraintInterface {
@@ -20,26 +20,26 @@ export const newGame = () => {
 }
 
 // give a square a hit value to be able to change class for css styling
-export const hit = (board, x, y) => {
+export const hit = (board: Board, x: number, y: number) => {
   board[x][y] += 'x'
   return board
 }
 
 // check if the hit is a ship and turn true if it is
-export const isShip = (board, x, y) => {
+export const isShip = (board: Board, x: number, y: number) => {
   const value = board[x][y]
   return !value.includes('0')
 }
 
 // check how many times a value is present in a row
-export const numberOfValues = (row, value) => {
+export const numberOfValues = (row: Row, value: string) => {
   return row
     .filter(v => v === value)
     .length
 }
 
 // see if all of the ships squares have been hit
-export const shipIsDestroyed = (board, value, shiplength) => {
+export const shipIsDestroyed = (board: Board, value: string, shiplength: number) => {
   const count = board
     .map(r => numberOfValues(r, value))
     .reduce((sum, i) => sum + i, 0)
@@ -47,7 +47,7 @@ export const shipIsDestroyed = (board, value, shiplength) => {
 }
 
 // one player won if all ships are destroyed
-export const gameWon = (board) => {
+export const gameWon = (board: Board) => {
   return (shipIsDestroyed(board, '1x', 2) &&
       shipIsDestroyed(board, '2x', 3) &&
       shipIsDestroyed(board, '3x', 3) &&
